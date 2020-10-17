3c2255b09149bd23c585f60ca034c4c1e5f22e81
РассадинИВ@ngknn.local <@ngknn.local>
2020-10-17 13:01:14 +0300

    Добавьте файлы проекта.

---
+++ DataBase/pages/PageLogin.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBase.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            int pass = psbPassword.Password.GetHashCode();
+            User UserObject = BaseConnect.BaseModel.User.FirstOrDefault(u => u.Login == txbLogin.Text && u.Password == pass);
+            if (UserObject == null)
+            {
+                MessageBox.Show("Такого пользователя нет");
+            }
+            else
+            {
+                switch (UserObject.Key_Role)
+                {
+                    case 1:
+                        MessageBox.Show("Вы вошли как администратор");
+                        break;
+                    default:
+                        MessageBox.Show("Системная ошибка");
+                        break;
+                }
+            }
+            
+        }
+    }
+}
